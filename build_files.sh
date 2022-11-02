91b8855dfdca4dcb1321d3a1c183bda81f764a73
Gabriela <dev.gabrielaellen@gmail.com>
2022-11-01 23:17:29 -0300

    correções no requeriments.txt

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,8 @@
 # build_files.sh
 pip install -r requirements.txt
-pip install python==3.9
 cd venv
 cd Scripts
 activate
 cd ..
 cd .. 
-python manage.py collectstatic
+python3.9 manage.py collectstatic
